9107389987933a156d3d25561454a3129f8d3fa9
servnnt <95766326+servnnt@users.noreply.github.com>
2022-01-14 11:51:53 +0000

    qdas

---
+++ script.js
@@ -0,0 +1,6 @@
+var hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", function(){
+     document.querySelector(".sidebar").classList.toggle("show-menu");
+});
+ 
