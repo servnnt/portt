f805acec5a420fc1153e4bfce4f428427358b4df
servnnt <95766326+servnnt@users.noreply.github.com>
2022-01-14 12:14:15 +0000

    wdasd

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var hamburguer = document.querySelector(".hamburguer");
 
 hamburguer.addEventListener("click", function(){
      document.querySelector(".sidebar").classList.toggle("show-menu");
-});
+    });
  
