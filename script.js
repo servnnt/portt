02292890ea827ac034959ed926fbace6ace8ed66
servnnt <95766326+servnnt@users.noreply.github.com>
2022-01-19 09:40:36 +0000

    :package: menu sidebar

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-var hamburguer = document.querySelector(".hamburguer");
-
-hamburguer.addEventListener("click", function(){
-    document.querySelector(".container").classList.toggle("show-menu");
-    });
+document.querySelector(".hamburguer").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+    );
  
